7513b236337c541d406610794dbe4b6b134f7e52
AislinnRay <aislinn.ray.m@gmail.com>
2020-05-20 16:46:34 -0600

    making progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isMobileMenuOpen: false,
+    }
+  }
+
+  toggleMobileMenu = () => {
+    this.setState({
+      isMobileMenuOpen: !this.state.isMobileMenuOpen,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <div className="nav-container">
+          <div className="logo-container">
+            <img
+              alt="Logo"
+              src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"
+              className="logo-img"/>
+          </div>
+          <nav className="nav">
+              <a className="nav-list-item" href="#">SERVICES</a>
+              <a className="nav-list-item" href="#">PORTFOLIO</a>
+              <a className="nav-list-item" href="#">ABOUT</a>
+              <a className="nav-list-item" href="#" >TEAM</a>
+              <a className="nav-list-item" href="#">CONTACT</a>
+          </nav>
+          {/* <nav className="mobile-nav">
+            <img
+              className="hamburger"
+              src="https://pngimage.net/wp-content/uploads/2018/06/white-menu-icon-png-8.png"
+              alt="Hamburger"
+              onClick={this.toggleMobileMenu}
+            />
+            <ul
+              className={`mobile-nav-list ${
+                this.state.isMobileMenuOpen ? 'mobile-nav-list-open' : null
+              }`}
+            >
+              <a className="mobile-nav-list-item" href="#">SERVICES</a>
+              <a className="mobile-nav-list-item" href="#">PORTFOLIO</a>
+              <a className="mobile-nav-list-item" href="#">ABOUT</a>
+              <a className="mobile-nav-list-item" href="#">TEAM</a>
+              <a className="mobile-nav-list-item" href="#">CONTACT</a>
+            </ul>
+          </nav> */}
+          </div>
+        </header>
+        <div className="hero-text">
+          <h1>
+            <span className="welcome">Welcome To Our Studio!</span><br/><span className="greeting">IT'S NICE TO MEET YOU</span>
+          </h1>
+          <button className="more-box" href="#">TELL ME MORE</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
